a41975eb816a230d4c2a3e8cb3da1c0f66bd88d0
luhaikong <1031350293@qq.com>
2016-10-09 10:40:37 +0800

    Initial commit

---
+++ IosDialog/src/main/java/com/adorkable/iosdialog/svprogresshud/listener/OnDismissListener.java
@@ -0,0 +1,10 @@
+package com.adorkable.iosdialog.svprogresshud.listener;
+
+import com.adorkable.iosdialog.svprogresshud.SVProgressHUD;
+
+/**
+ * Created by Sai on 16/7/31.
+ */
+public interface OnDismissListener {
+    void onDismiss(SVProgressHUD hud);
+}
---
+++ app/src/main/java/com/adorkable/iosdialogdemo/SecondActivity.java
@@ -0,0 +1,99 @@
+package com.adorkable.iosdialogdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.adorkable.iosdialog.svprogresshud.SVProgressHUD;
+import com.adorkable.iosdialog.svprogresshud.listener.OnDismissListener;
+
+/**
+ * Created by lhk on 2016/10/9.
+ */
+public class SecondActivity extends Activity {
+    private SVProgressHUD mSVProgressHUD;
+    int progress = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mSVProgressHUD = new SVProgressHUD(this);
+        mSVProgressHUD.setOnDismissListener(new OnDismissListener(){
+            @Override
+            public void onDismiss(SVProgressHUD hud) {
+                // todo something, like: finish current activity
+                Toast.makeText(getApplicationContext(),"dismiss",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void show(View view){
+        mSVProgressHUD.show();
+    }
+    public void showWithMaskType(View view){
+        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.None);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.Black);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.BlackCancel);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.Clear);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.ClearCancel);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.Gradient);
+//        mSVProgressHUD.showWithMaskType(SVProgressHUD.SVProgressHUDMaskType.GradientCancel);
+    }
+    public void showWithStatus(View view){
+        mSVProgressHUD.showWithStatus("加载中...");
+    }
+    public void showInfoWithStatus(View view){
+        mSVProgressHUD.showInfoWithStatus("这是提示", SVProgressHUD.SVProgressHUDMaskType.None);
+    }
+    public void showSuccessWithStatus(View view){
+        mSVProgressHUD.showSuccessWithStatus("恭喜，提交成功！");
+    }
+    public void showErrorWithStatus(View view){
+        mSVProgressHUD.showErrorWithStatus("不约，叔叔我们不约～", SVProgressHUD.SVProgressHUDMaskType.GradientCancel);
+    }
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            progress = progress + 5;
+            if (mSVProgressHUD.getProgressBar().getMax() != mSVProgressHUD.getProgressBar().getProgress()) {
+                mSVProgressHUD.getProgressBar().setProgress(progress);
+                mSVProgressHUD.setText("进度 "+progress+"%");
+
+                mHandler.sendEmptyMessageDelayed(0,500);
+            }
+            else{
+                mSVProgressHUD.dismiss();
+            }
+
+        }
+    };
+    public void showWithProgress(View view){
+        progress = 0;
+        mSVProgressHUD.getProgressBar().setProgress(progress);//先重设了进度再显示，避免下次再show会先显示上一次的进度位置所以要先将进度归0
+        mSVProgressHUD.showWithProgress("进度 " + progress + "%", SVProgressHUD.SVProgressHUDMaskType.Black);
+        mHandler.sendEmptyMessageDelayed(0,500);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
+        {
+            if(mSVProgressHUD.isShowing()){
+                mSVProgressHUD.dismiss();
+                mHandler.removeCallbacksAndMessages(null);
+                return false;
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
+
+    }
+}
